f29a9e4d3878dcb809c20e67b5de71a89696506a
Harsh Jain <harshjainldh@gmail.com>
2024-04-03 20:50:04 +0530

    Update kelvin_Voigt_withFriction.m

--- kelvin_Voigt_withFriction.m
+++ kelvin_Voigt_withFriction.m
@@ -6,7 +6,7 @@ k = 50000;   % Elastic modulus in Pa
 eta = 20000; % Viscosity in Pa.s
 
 % Time parameters
-dt = 1e-3; % Time step in seconds
+dt = 1e-1; % Time step in seconds
 t_mat = 0:dt:10; % Total time for simulation in seconds
 
 % Total number of time steps
@@ -16,7 +16,7 @@ N = length(t_mat);
 halfN = round(N/2)-1;
 %%
 
-maxStrain=0.1
+maxStrain=0.1;
 epsilon_mat = linspace(0,maxStrain,halfN+1); % Strain
 %epsilon_mat(end+1) = epsilon_mat(halfN);
 
@@ -27,7 +27,7 @@ epsilon_dot_mat(end+1) = -1*epsilon_dot_old;
 %%
 k_mat = k*epsilon_mat(1:halfN).^2;          % Corresponding values of k
 eta_mat=eta*epsilon_mat(1:halfN);
-fric_mat = 4000.*epsilon_mat(1:halfN).^2;
+fric_mat = 90000.*epsilon_mat(1:halfN).^2;
 
 
 % Create an interpolation functions
@@ -42,88 +42,36 @@ sigma_mat = zeros(1,halfN);
 close all
 sigma_mat(1:halfN)=k_mat.*epsilon_mat(1:halfN)...
         + eta_mat.*epsilon_dot_mat(1:halfN)...
-        -fric_mat.*epsilon_mat(1:halfN);
+        -fric_mat.*epsilon_dot_mat(1:halfN);
 
 plot(epsilon_mat(1:halfN),sigma_mat(1:halfN));
 hold on; plot(epsilon_mat(1:halfN),sigma_mat(1:halfN)./epsilon_mat(1:halfN))
 
 %% Return Cycle
-n =(length(epsilon_mat)+1); 
-t =t_mat(n); 
-epsilon_current = epsilon_mat(end);
-epsilon_dot_current= epsilon_dot_mat(end);
-k_current =     k_interp(epsilon_current);
-eta_current = eta_interp(epsilon_current);
-fric_current = fric_interp(epsilon_current);
-%eta_dt = eta_current *
-DecayT = eta_current./k_current;
+tMat2 = [];
+t2= 0;
+%%
+for i = 1: 200
+epsilon0 = epsilon_mat(end);
 
-epsilon_new = epsilon_current*exp(-dt/DecayT);
-epsilon_dot_new = (epsilon_new -epsilon_current)/dt;
+eta_current  = eta_interp(epsilon0);
+k_current    = k_interp(epsilon0);
+fric_current = fric_interp(epsilon0);
+DecayT = eta_current./(k_current+fric_current);
 
-    while (epsilon_dot_new < epsilon_dot_old )
-        epsilon_mat(end+1) = epsilon_mat(end) + dt*epsilon_dot_old;
-        sigma_mat(end+1) = k_current*epsilon_mat(end)+ ...
-            eta_current*epsilon_dot_mat(end)...
-            - fric_current*epsilon_mat(end);
-        epsilon_dot_mat(end+1) = epsilon_dot_old;
+epsilon_relax = epsilon0*exp(-dt/DecayT);
 
-        n =(length(epsilon_mat)+1); 
+epsilon_dot_relax = epsilon_relax-epsilon_mat(end);
 
-        t =t_mat(n); 
-        epsilon_current = epsilon_mat(end);
-        epsilon_dot_current= epsilon_dot_mat(end);
-        k_current =     k_interp(epsilon_current);
-        eta_current = eta_interp(epsilon_current);
-        %eta_dt = eta_current *
-        DecayT = eta_current./k_current;
-
-        epsilon_new = epsilon_current*exp(-dt/DecayT)
-        epsilon_dot_new = (epsilon_new -epsilon_current)/dt
-
+sigma_relax   =   k_current.*epsilon_relax...
+                + eta_current.*epsilon_dot_relax...
+                + fric_current.*epsilon_dot_relax;
+epsilon_mat(end+1) = epsilon_relax;
+sigma_mat(end+1) = sigma_relax;
+epsilon_dot_mat(end+1) = epsilon_dot_relax;
+t2 = t2+dt;
+tMat2(end+1) = t2;
 end
-%% Return cycle second half
 
-n =(length(epsilon_mat)+1); 
-t =t_mat(n); 
-epsilon_current = epsilon_mat(end);
-epsilon_dot_current= epsilon_dot_mat(end);
-k_current =     k_interp(epsilon_current);
-eta_current = eta_interp(epsilon_current);
-%eta_dt = eta_current *
-DecayT = eta_current./k_current;
-
-epsilon_new = 0.1*exp(-dt/DecayT);
-epsilon_dot_new = (epsilon_new -epsilon_current)/dt;
-
-plot(epsilon_mat(5002:end),sigma_mat(5001:end))
-
-
-%% Plotting
-close all
-em=transpose(epsilon_mat(1:end-1));
-sm=sigma_mat;
-%plot(em,sm./em)
 %%
-sm1 = sm(1:5000);
-em1 = em(1:5000);
-
-sm2=sm(5001:end);
-em2=em(5001:end);
-%sm3(1:10)=linspace(0.699,0.3,10)
-%em3=[0.1*ones(10,1); em2];
-
-tMat2 =linspace(0,5*DecayT,1000); 
-em4=em2(end)*exp(-tMat2/DecayT);
-
-dem4=(diff(em4)./diff(tMat2));
-em4_ = em4(2:end);
-sm4 = k_interp(em4_).*em4_+ ...
-           eta_interp(em4_).*dem4...
-           - fric_interp(em4_).*em4_;
-% sm4 = -fric_interp(em4).*em4+k_interp(em4).*em4;
-%+eta_interp(em4).*epsilon_dot_new;
-em5=[em;em4'];
-sm5=[sm sm4];
-%%
-plot(sm5)
+plot(epsilon_mat(2:end),sigma_mat,'o--')

d8d3ade0e4f3b731658204bcff187ea7fcf00b1c
Harsh Jain <harshjainldh@gmail.com>
2024-04-13 16:05:45 +0530

    Update kelvin_Voigt_plain_1.m

--- kelvin_Voigt_plain_1.m
+++ kelvin_Voigt_plain_1.m
@@ -17,55 +17,55 @@ halfN = round(N/2)-1;
 %%
 
 maxStrain=0.1;
-epsilon_mat = linspace(0,maxStrain,halfN+1); % Strain
-%epsilon_mat(end+1) = epsilon_mat(halfN);
+x_mat = linspace(0,maxStrain,halfN+1); % Strain
+%x_mat(end+1) = x_mat(halfN);
 
-epsilon_dot_mat = diff(epsilon_mat(1:halfN+1))/dt; % Strain rate
-%epsilon_dot_mat(end+1) =0;
+x_dot_mat = diff(x_mat(1:halfN+1))/dt; % Strain rate
+%x_dot_mat(end+1) =0;
 
 %%
-k_mat = k*epsilon_mat(1:halfN).^2;          % Corresponding values of k
-eta_mat=eta*epsilon_mat(1:halfN);
+k_mat = k*x_mat(1:halfN).^2;          % Corresponding values of k
+eta_mat=eta*x_mat(1:halfN);
 
 
 % Create an interpolation functions
-k_interp = @(x) interp1(epsilon_mat(1:halfN), k_mat, x, 'linear', 'extrap');
-eta_interp = @(x) interp1(epsilon_mat(1:halfN),eta_mat,x,'linear','extrap');
+k_interp = @(x) interp1(x_mat(1:halfN), k_mat, x, 'linear', 'extrap');
+eta_interp = @(x) interp1(x_mat(1:halfN),eta_mat,x,'linear','extrap');
 
 % Initializing strain and strain rate arrays
-sigma_mat = zeros(1,halfN);
+f_mat = zeros(1,halfN);
 %% Forward Cycle
 % Simulation loop for the Kelvin-Voigt model
 
-sigma_mat(1:halfN)=k_mat.*epsilon_mat(1:halfN)...
-        + eta_mat.*epsilon_dot_mat(1:halfN);
+f_mat(1:halfN)=k_mat.*x_mat(1:halfN)...
+        + eta_mat.*x_dot_mat(1:halfN);
 
-plot(sigma_mat(1:halfN));
-hold on; plot(sigma_mat(1:halfN)./epsilon_mat(1:halfN))
+plot(f_mat(1:halfN));
+hold on; plot(f_mat(1:halfN)./x_mat(1:halfN))
 
 %% Return Cycle
 tMat2 = [];
 t2= 0;
 %%
 for i = 1: 200
-epsilon0 = epsilon_mat(end);
+x0 = x_mat(end);
 
-eta_current = eta_interp(epsilon0);
-k_current = k_interp(epsilon0);
-DecayT = eta_current./k_current;
+eta_eff = eta_interp(x0);
+k_eff = k_interp(x0);
+DecayT = eta_eff./k_eff;
 
-epsilon_relax = epsilon0*exp(-dt/DecayT);
+x_relax = x0*exp(-dt/DecayT);
 
-epsilon_dot_relax = epsilon_relax-epsilon_mat(end);
+x_dot_relax = x_relax-x_mat(end);
 
-sigma_relax=   k_current.*epsilon_relax...
-        + eta_current.*epsilon_dot_relax;
-epsilon_mat(end+1) = epsilon_relax;
-sigma_mat(end+1) = sigma_relax;
-epsilon_dot_mat(end+1) = epsilon_dot_relax;
+f_relax=   k_eff.*x_relax...
+        + eta_eff.*x_dot_relax;
+x_mat(end+1) = x_relax;
+f_mat(end+1) = f_relax;
+x_dot_mat(end+1) = x_dot_relax;
 t2 = t2+dt;
 tMat2(end+1) = t2;
 end
 
 %%
-plot(epsilon_mat(2:end),sigma_mat,'o')
+plot(x_mat(2:end),f_mat,'o')
